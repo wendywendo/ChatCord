cf6b608b45800da662861050fb1dfab963973a86
wendywendo <wendywendo06@gmail.com>
2025-02-23 22:26:27 +0300

    Removed unused variables

--- client/src/components/Chats/Chats.jsx
+++ client/src/components/Chats/Chats.jsx
@@ -11,12 +11,10 @@ function Chats() {
     const { messages, setMessages, fetchMessages, loadingMessages, activeRoom, user } = useAuth()
     const [message, setMessage] = useState('')
     const [typing, setTyping] = useState(false)
-    const [typingUser, setTypingUser] = useState("")
+    const [setTypingUser] = useState("")
     const inputRef = useRef(null)
     const messagesEndRef = useRef(null)
     let typingTimeout = useRef(null)
-
-    const apiUrl = import.meta.env.VITE_API_URL
 
 
     useEffect(() => {
